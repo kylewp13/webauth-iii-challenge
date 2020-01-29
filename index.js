07ab302b59928bc22128126fdc66fa83e04b171d
Kyle Porter <kyleporter@Kyles-Air.attlocal.net>
2020-01-29 11:47:16 -0500

    setup server

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const server = require('./server');
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`\n ** server running on http://localhost:${port} ** \n`)
+})
